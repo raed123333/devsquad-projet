c27f9af53dd917bc9e8ca4cd272bf2336471a3b6
JABRI Raed <jabriraed80@gmail.com>
2024-11-09 12:17:29 +0100

    Integrate backend API into frontend on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React , {useState , useEffect }from 'react'
+import axios from 'axios';
+import Spinner from '../components/Spinner';
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox , MdOutlineDelete} from'react-icons/md';
 
 const Home = () => {
+  const [employees, setEmployees] = useState([]);
+  const[loading,setLoading] = useState(false);
+  useEffect(() => {
+    axios.get('http:// localhost:8089/Employee')
+    .then((response) =>{
+      setEmployees(response.data.data);
+      setLoading(false);
+    })
+    .catch((error) => {
+      console.log(error);
+      setLoading(false);
+    })
+  },[])
   return (
     <div>Home</div>
   )
