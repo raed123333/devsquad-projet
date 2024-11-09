0c539eb90c9aacddb115c1e5c9952997305f476f
JABRI Raed <jabriraed80@gmail.com>
2024-11-09 11:39:54 +0100

    add all the routes in a page App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
+import CreateEmployee from'./pages/CreateEmployee';
+import DeleteEmployee from'./pages/DeleteEmployee';
+import EditEmployee from './pages/EditEmployee';
+import Home from './pages/Home';
+import ShowEmployee from './pages/ShowEmployee';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={} />
-      <Route path="/" element={} />
-      <Route path="/" element={} />
-      <Route path="/" element={} />
-      <Route path="/" element={} />
+      <Route path="/" element={<Home/>} />
+      <Route path="/employees/craete" element={<CreateEmployee/>} />
+      <Route path="/employees/delete/:id" element={<DeleteEmployee/>} />
+      <Route path="/employees/edit/:id" element={<EditEmployee/>} />
+      <Route path="/employees/details/:id" element={<ShowEmployee/>} />
     </Routes>
   )
 }
