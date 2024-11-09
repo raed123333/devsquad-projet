8f9eefc3de2f938a7f4d0d41fe7fb6f09429a289
JABRI Raed <jabriraed80@gmail.com>
2024-11-09 13:36:08 +0100

    i start create page home i build table for employee

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineAddBox , MdOutlineDelete} from'react-icons/md';
 
+
 const Home = () => {
   const [employees, setEmployees] = useState([]);
   const[loading,setLoading] = useState(false);
@@ -21,7 +22,69 @@ const Home = () => {
     })
   },[])
   return (
-    <div>Home</div>
+    <div className='p-4'>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-3xl my-8'> liste des employés</h1>
+        <Link to='/employees/craete' >
+        <MdOutlineAddBox className='text-sky-800 text-4xl' />
+        </Link>
+      </div>
+      {loading ?(
+        <Spinner/>
+
+      ):(
+        <table className='w-full border-separate border-spacing-2'>
+          <thead>
+            <tr>
+              <th className='border border-slate-600 rounded-md'>No</th>
+              <th className='border border-slate-600 rounded-md'>Nom</th>
+              <th className='border border-slate-600 rounded-md'>Prenom</th>
+              <th className='border border-slate-600 rounded-md'>email</th>
+              <th className='border border-slate-600 rounded-md'>cin</th>
+              <th className='border border-slate-600 rounded-md'>age</th>
+              <th className='border border-slate-600 rounded-md'>tel</th>
+              <th className='border border-slate-600 rounded-md'>poste</th>
+              
+            </tr>
+
+          </thead>
+          <tbody>
+            {employees.map( (employee,index)=>
+            <tr key={employee._id} className='h-8'>
+              <td className='border border-slate-700 rounded-md text-center'>
+                {index+1}
+              </td>
+              <td className='border border-slate-700 rounded-md text-center'>
+                {employee.nom}
+              </td>
+              <td className='border border-slate-700 rounded-md text-center'>
+                {employee.prenom}
+              </td>
+              <td className='border border-slate-700 rounded-md text-center'>
+                {employee.email}
+              </td>
+              <td className='border border-slate-700 rounded-md text-center'>
+                {employee.cin}
+              </td>
+              <td className='border border-slate-700 rounded-md text-center'>
+                {employee.numPhone}
+              </td>
+              <td className='border border-slate-700 rounded-md text-center'>
+                {employee.poste}
+              </td>
+
+            </tr>
+            
+            
+            )}
+
+          </tbody>
+
+        </table>
+
+      )}
+
+    </div>
   )
 }
 
